6f59f8217f7306d44f07c7cbd6b91d46a1a2bf29
Shavit Siri <shavit279@gmail.com>
2023-10-23 16:02:55 +0300

    last fixes

--- Screens/ResultsScreen.tsx
+++ Screens/ResultsScreen.tsx
@@ -23,11 +23,10 @@ const ResultsScreen: React.FC = () => {
             const resultsData = await AsyncStorage.getItem('results');
             if (resultsData !== null) {
                 const results_user = JSON.parse(resultsData);
-                const sortedResults = results_user.sort((a, b) => b.score - a.score);
+                const sortedResults = results_user.sort((a: { score: number }, b: { score: number }) => b.score - a.score);
                 setResults(sortedResults);
             }
-        } 
-        catch (error) {
+        } catch (error) {
             console.error('Error retrieving data from AsyncStorage:', error);
         }
     };
@@ -36,18 +35,13 @@ const ResultsScreen: React.FC = () => {
         getResultsFromAsync();
     });
 
-
-
-    // Get only the top 10 results
     const top10Results = results.slice(0, 10);
-  
+
     return (
-        <View style={styles.container}>
-            <View style={styles.holder_header} >
-                <Text style={styles.text_header} > LeaderBoard </Text>
+        <View style={styles.Container}>
+            <View style={styles.header}>
+                <Text style={styles.headerText}>Leaderboard</Text>
             </View>
-            <Image style={styles.icon_confetti_1} source={require('../images/icon_confetti.png')} />
-            <Image style={styles.icon_confetti_2} source={require('../images/icon_confetti.png')} />
             <FlatList
                 data={top10Results}
                 keyExtractor={(_, index) => index.toString()}
@@ -57,102 +51,68 @@ const ResultsScreen: React.FC = () => {
                             style={styles.resultImage}
                             source={
                                 index === 0
-                                ? require('../images/thropy_gold.jpg')
-                                : index === 1
-                                ? require('../images/thropy_silver.jpeg')
-                                : require('../images/thropy_bronze.jpeg')
+                                    ? require('../images/thropy_gold.jpg')
+                                    : index === 1
+                                    ? require('../images/thropy_silver.jpeg')
+                                    : require('../images/thropy_bronze.jpeg')
                             }
                         />
-                        <Image style={styles.icon_crown} source={ index === 0 ? require('../images/icon_crown.png') : null } />
-                        <Text style={styles.resultName}>{` ${item.name.length? item.name : 'Guest'}`}</Text>
+                        <Text style={styles.resultName}>{`${item.name.length ? item.name : 'Guest'}`}</Text>
                         <Text style={styles.resultScore}>{`Score: ${item.score}`}</Text>
                     </View>
                 )}
             />
-            <TouchableOpacity style={styles.newGameButton} onPress={() => { navigation.navigate('Game'); handleSetOtherValue(); }} >
+            <TouchableOpacity style={styles.newGameButton} onPress={() => { navigation.navigate('Game'); handleSetOtherValue(); }}>
                 <Text style={styles.newGameButtonText}>Start New Game</Text>
             </TouchableOpacity>
         </View>
     );
-
-
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+    Container:{
+        flex:1,
         backgroundColor: '#343539',
     },
-    holder_header:{
-        padding: 16, // Adjust the padding as needed
+    header: {
+        padding: 20,
     },
-    icon_confetti_1:{
-        position:'absolute',
-        top:35,
-        left:35,
-    },
-    icon_confetti_2:{
-        position:'absolute',
-        top:35,
-        right:35,
-    },
-    text_header:{
-        fontSize: 24,
+    headerText: {
+        fontSize: 28,
         fontWeight: 'bold',
-        color: 'white', // Text color
-        textAlign: 'center',
-    },
-    resultsText: {
-        fontSize: 24,
-        marginBottom: 15,
-        marginTop:5,
-        color:'white',
+        color: 'white',
     },
     resultItem: {
-        marginBottom: 10,
         flexDirection: 'row',
         justifyContent: 'space-between',
-        width:250,
-        margin:5,
-        backgroundColor:'aliceblue',
-        borderRadius:12,
-        height:35,
+        alignItems: 'center',
+        margin: 10,
+        backgroundColor: 'rgba(255, 255, 255, 0.7)',
+        padding: 10,
+        borderRadius: 10,
+    },
+    resultImage: {
+        width: 40,
+        height: 40,
     },
     resultName: {
-        fontSize: 16,
-        color:'black',
-        marginTop:5,
+        fontSize: 18,
+        color: 'black',
     },
     resultScore: {
-        fontSize: 16,
-        color:'black',
-        marginTop:5,
-        marginRight:30,
-    },
-    resultImage:{
-        width:35,
-        height:35,
-        borderRadius:12,
-    },
-    icon_crown:{
-        position:'absolute',
-        right:0,
-        width:25,
-        height:25,
-        transform: [{ rotate: '45deg' }],
+        fontSize: 18,
+        color: 'black',
     },
     newGameButton: {
         backgroundColor: '#4CAF50',
-        padding: 6,
-        borderRadius: 5,
-        marginBottom:15,
-
+        padding: 10,
+        borderRadius: 10,
+        marginTop: 20,
     },
     newGameButtonText: {
         color: 'white',
-        fontSize: 18,
+        fontSize: 20,
+        textAlign: 'center',
     },
 });
 
